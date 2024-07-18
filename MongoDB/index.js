008cd3106425ac9ed9e28aea00af117cb60814da
Wagner Bocchi <wagner@bocchisecurity.com>
2024-07-18 10:16:40 -0300

    Primeiro update

---
+++ MongoDB/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/esmtickets', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connected to mongoDB');
+});
